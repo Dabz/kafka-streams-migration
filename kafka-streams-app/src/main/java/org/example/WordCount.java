f1b1c65c7a273e3291441700719cd38dfde8948a
Damien Gasparina <d.gasparina@gmail.com>
2021-12-07 18:18:48 +0100

    upgrade to CP 7.0 and changed applicatin.id while migration

--- kafka-streams-app/src/main/java/org/example/WordCount.java
+++ kafka-streams-app/src/main/java/org/example/WordCount.java
@@ -19,6 +19,7 @@ package org.example;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.config.TopicConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
@@ -47,11 +48,11 @@ public class WordCount {
 
     public static void main(String[] args) throws Exception {
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE_BETA);
+        props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE_V2);
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
         if (args.length > 0) {
             props.load(new FileInputStream(args[0]));
         }
@@ -72,7 +73,7 @@ public class WordCount {
                 .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"))
                 .toStream()
                 .mapValues((k, v) -> k + " " + String.valueOf(v))
-                .to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.String()));
+                .to("streams-plaintext-output", Produced.with(Serdes.String(), Serdes.String()));
 
         final Topology topology = builder.build();
         final KafkaStreams streams = new KafkaStreams(topology, props);
